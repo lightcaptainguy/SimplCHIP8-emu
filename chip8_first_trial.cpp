7d334ec9334aaac80376c8785133c36db67ce1e3
lightcaptainguy <130440682+lightcaptainguy@users.noreply.github.com>
2026-02-23 00:28:28 +0530

    First Trial implementing dummy bytes
    Loading dummy bytes 0x12 and 0x34 to memory which combine to form opcode 0x1234 which basically states that jump to memory location 0x234

---
+++ chip8_first_trial.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+#include <cstdint>
+#include <fstream>
+using namespace std;
+int main ()
+{ 
+    uint8_t memory[4096] = {0};
+    uint8_t V[16] = {0};
+    uint16_t I = {0};
+    uint16_t pc = 0x200;
+    
+    memory[0x200] = 0x12;
+    memory[0x201] = 0x34;
+    
+    uint16_t opcode = memory[pc] << 8 | memory[pc+1];
+    std::cout<<"CHIP-8 Initialized\n";
+    std::cout<<"PC: 0x" <<std::hex << pc << std::endl;
+    std::cout<<"Opcode at PC: 0x" <<std::hex << opcode << std::endl;
+
+
+
+}
+
+
+
