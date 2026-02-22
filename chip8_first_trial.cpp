37dc19868c66825ca2da4d91530baf7882d3fe96
lightcaptainguy <130440682+lightcaptainguy@users.noreply.github.com>
2026-02-23 01:21:37 +0530

    added a few comments
    no change, just added a few comments

--- chip8_first_trial.cpp
+++ chip8_first_trial.cpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <fstream>
 using namespace std;
+
 void executeOpcode(uint16_t opcode, uint16_t &pc) {
     uint16_t firstNibble = (opcode & 0xF000) >> 12;
     if (firstNibble == 0x1) {
@@ -11,16 +12,16 @@ void executeOpcode(uint16_t opcode, uint16_t &pc) {
         std::cout<< "Jump to 0x" << std::hex << address << std::endl;
     }
 } 
+
 int main ()
 { 
-    uint8_t memory[4096] = {0};
-    uint8_t V[16] = {0};
-    uint16_t I = {0};
+    uint8_t memory[4096] = {0}; // RAM
+    uint8_t V[16] = {0}; // 16 8 bit memory registers Vx, where x varies from 0 to F, unused for now
+    uint16_t I = {0}; // Index register I, used to point locations in memory, unused for now
     uint16_t pc = 0x200;
     
     memory[0x200] = 0x12;
     memory[0x201] = 0x34;
-
     uint16_t opcode = memory[pc] << 8 | memory[pc+1];
     
     std::cout<<"CHIP-8 Initialized\n";
