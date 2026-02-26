a75b1fc7bf5a78e8114cf3c6839647326e0d4dfd
lightcaptainguy <130440682+lightcaptainguy@users.noreply.github.com>
2026-02-27 00:36:42 +0530

    Implemented the header file, created skeleton functions, and set registers
    Added memory and register declarations
    Implement display and input buffers
    added skeleton init method, exec stub, cycle , and loadrom methods

--- chip8.h
+++ chip8.h
@@ -2,6 +2,27 @@
 #define CHIP8_H
 #include <cstdint>
 
-class CHIP8_H {
+class chip8 {
+    public:
+    chip8();
+    void emulateCycle();
+    void loadROM(filename const char* filename);
+    uint8_t display[64 * 32];
+    uint8_t keys[16];
+    bool drawFlag;
     
+    private:
+    uint8_t memory[4096]; // RAM 
+    uint8_t V[16]; // 16 8 bit V registers
+    uint16_t I; // 16 bit register I
+    uint16_t pc; //16 bit program counter
+    uint8_t SP; //8 bit stack counter
+    uint8_t stack[16]; //16 16bit values can be stored in the stack
+    uint8_t delay_timer, sound_timer; // delay and sound 8 bit registers
+
+    void Initialize()
+    void executeOpcode(uint16_t opcode)
+
+
+
 }
